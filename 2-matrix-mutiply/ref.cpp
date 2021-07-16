570565f88f8fb21cab990dcd636791558aa8f80c
SMagic-L <38354551+SMagic-L@users.noreply.github.com>
2021-07-16 15:21:11 +0800

    修复了matrix的ref的小bug

--- 2-matrix-mutiply/ref.cpp
+++ 2-matrix-mutiply/ref.cpp
@@ -14,7 +14,7 @@ string ref(const MatArray& left, const MatArray& right, MatArray& result)
         parallel_for (int k = 0; k < result.size; k++)
             for(int j = 0; j < right.size; j++)
             {
-                result.data[i][j] += left.data[i][k] + right.data[k][j];
+                result.data[i][j] += left.data[i][k] * right.data[k][j];
             }
 
     return func_name;
